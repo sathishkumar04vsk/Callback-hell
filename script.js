fbe4e1cf88d05c6bb6a73328412ab73a16009af0
Sathishkumar <90319720+sathishkumar04vsk@users.noreply.github.com>
2022-01-03 16:34:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+window.onload=function(){
+    var txt=document.getElementById('text');
+    var msg=document.getElementById('message');
+    msg.style.visibility='hidden';
+    txt.innerHTML=10;
+    setTimeout(()=>{
+        txt.innerHTML=9;
+        setTimeout(()=>{
+            txt.innerHTML=8;
+            setTimeout(()=>{
+                txt.innerHTML=7;
+                setTimeout(()=>{
+                    txt.innerHTML=6;
+                    setTimeout(()=>{
+                        txt.innerHTML=5;
+                        setTimeout(()=>{
+                            txt.innerHTML=4;
+                            setTimeout(()=>{
+                                txt.innerHTML=3;
+                                setTimeout(()=>{
+                                    txt.innerHTML=2;
+                                    setTimeout(()=>{
+                                        txt.innerHTML=1;
+                                        setTimeout(()=>{
+                                            msg.style.visibility='visible';
+                                            txt.style.visibility='hidden';
+                                        },2000)
+                                    },2000)
+                                },2000)
+                            },2000)
+                        },2000)
+                    },2000)
+                },2000)
+            },2000)
+        },2000)
+    },2000)
+}
